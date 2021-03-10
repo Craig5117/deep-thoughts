2542880983a84f23d1d952ff7e534a2b943c528f
Craig5117 <craig.bennett5117@gmail.com>
2021-03-10 14:11:45 -0500

    set up jwt and add thought, reaction, friend

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3002;
@@ -13,7 +14,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
