e4b4c0e367285e9740c5e4ab1fb141ce98aca156
Craig5117 <craig.bennett5117@gmail.com>
2021-03-10 22:43:18 -0500

    set up front-end and connected to backend

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2021 by Craig5117</div>
+    </footer>
+  );
+};
+
+export default Footer;
